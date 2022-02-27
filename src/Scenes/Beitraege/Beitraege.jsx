d7efe35d73133a58bf623d907eb0bd37861a1c4b
Ferdinand <strass.fred@gmail.com>
2022-02-27 11:13:40 +0100

    godlike Pagination finished ✨

--- src/Scenes/Beitraege/Beitraege.jsx
+++ src/Scenes/Beitraege/Beitraege.jsx
@@ -7,7 +7,7 @@ import BeitragCreator from "./BeitragCreator"
 import parseMdToHtml from "../../helper/parseMdToHtml"
 import useForceUpdate from "../../hooks/useForceUpdate"
 import DrillDownPath from "../../components/DrillDownPath/DrillDownPath"
-import Pagination from "../../components/Pagination/Pagination"
+import Pagination, { usePaginationState } from "../../components/Pagination/Pagination"
 
 export default function Beitraege() {
     let { idForum, idForeneintrag } = useParams()
@@ -16,11 +16,12 @@ export default function Beitraege() {
     const [beitragList, setBeitragList] = useState()
     const [trigger, forceUpdate] = useForceUpdate()
     //pagination data
-    const [limitOffset, setLimitOffset] = useState({ limit: 10, offset: 0 })
+    const paginationState = usePaginationState(61, null, 10)
+    const { limit, offset } = paginationState[0]
 
     useEffect(() => {
         function getData() {
-            getBeitraegeForForeneintrag(idForeneintrag, limitOffset.limit, limitOffset.offset)
+            getBeitraegeForForeneintrag(idForeneintrag, limit, offset)
                 .then((data) => {
                     setBeitragList(data)
                     console.log(data)
@@ -32,10 +33,10 @@ export default function Beitraege() {
         }
 
         getData()
-    }, [idForeneintrag, trigger, limitOffset])
+    }, [idForeneintrag, trigger, limit, offset])
 
     useEffect(() => {
-        getForeneintraegeById({ idForum: idForum, idForeneintrag: idForeneintrag })
+        getForeneintraegeById({ idForeneintrag: idForeneintrag })
             .then((data) => {
                 setForeneintragData(data)
                 console.log(data)
@@ -48,18 +49,16 @@ export default function Beitraege() {
             <DrillDownPath forumData={idForum} />
 
             <h1>{foreneintragData ? foreneintragData[0].name : null}</h1>
-            <Pagination
-                defaultLimit={limitOffset.limit}
-                elementCount={61}
-                handleChange={(limit, offset) => {
-                    setLimitOffset({ limit, offset })
-                }}
-            />
+
+            <Pagination externalPaginationState={paginationState} />
+
             <div className={styles.content}>
                 {beitragList
                     ? beitragList.map((beitrag) => <Beitrag key={beitrag.idBeitrag} parseMdToHtml={parseMdToHtml} beitragData={beitrag} />)
                     : console.log(beitragList)}
             </div>
+
+            <Pagination externalPaginationState={paginationState} />
 
             <BeitragCreator parseMdToHtml={parseMdToHtml} forceUpdateBeitraege={forceUpdate} idForum={idForum} idForeneintrag={idForeneintrag} />
         </div>
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,44 +1,52 @@
-import { Pagination as MuiPagination, TextField } from "@mui/material"
+import { MenuItem, Pagination as MuiPagination, Select } from "@mui/material"
 import { useState } from "react"
+import styles from "./Pagination.module.css"
 
-export function usePaginationState({ elementCount, startPage }) {
-    const [page, setPage] = useState(startPage || 1)
-    const [limit, setLimit] = useState(10)
+export function usePaginationState(elementCount, defaultPage, defaultLimit) {
+    const [pageState, setPageState] = useState({
+        page: defaultPage || 1,
+        limit: defaultLimit || 10,
+    })
 
+    const { page, limit } = pageState
+    const pageCount = Math.floor(elementCount / limit) || 1
     const offset = limit * (page - 1)
-    const pageCount = elementCount % limit
 
-    return { page, pageCount, limit, offset }
+    function updateState(obj) {
+        setPageState({ ...pageState, ...obj })
+    }
+
+    return [{ page, pageCount, limit, offset }, updateState]
 }
 
-export default function Pagination({ elementCount = 0, startPage, defaultLimit, handleChange }) {
-    const [page, setPage] = useState(startPage || 1)
-    const [limit, setLimit] = useState(defaultLimit || 10)
-
-    const offset = limit * (page - 1)
-    const pageCount = Math.floor(elementCount / limit) || 1
+export default function Pagination({ elementCount, defaultPage, defaultLimit, handleChange, externalPaginationState }) {
+    const [pageState, updateState] = externalPaginationState || usePaginationState(elementCount, defaultPage, defaultLimit)
+    const { page, pageCount, limit, offset } = pageState
 
     return (
-        <div>
+        <div className={styles.PaginationDiv}>
             <MuiPagination
                 page={page}
                 count={pageCount}
                 onChange={(e, page) => {
-                    setPage(page)
-                    const offset = limit * (page - 1)
-                    handleChange(limit, offset)
+                    updateState({ page })
+                    handleChange ? handleChange(limit, offset) : null
                 }}
                 shape="rounded"
             />
-            <TextField
-                id="standard-number"
-                label="Number"
-                type="number"
-                InputLabelProps={{
-                    shrink: true,
+            <Select
+                value={limit}
+                onChange={(e) => {
+                    updateState({ limit: e.target.value })
+                    handleChange ? handleChange(limit, offset) : null
                 }}
                 variant="standard"
-            />
+            >
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={50}>50</MenuItem>
+                <MenuItem value={100}>100</MenuItem>
+            </Select>
         </div>
     )
 }
